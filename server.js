9a22207473281a885de9a86988b927277bd7abab
Franco Traverso <franco-t20@outlook.com>
2021-04-21 20:37:55 -0300

    add mock up endpoint to test http requests

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('localhost for now')
+})
+
 const PORT = process.env.PORT || 5000 //first looks for a PORT variable, if not present, use the localhost port
 
 app.listen(PORT, () => {
