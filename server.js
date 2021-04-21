48aec00985339e87f03b94f7abf44c97e42e58c1
Franco Traverso <franco-t20@outlook.com>
2021-04-21 20:40:35 -0300

    set up json response

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('localhost for now')
+    res.json({ msg: 'ContactKeeper API'})
 })
 
-const PORT = process.env.PORT || 5000 //first looks for a PORT variable, if not present, use the localhost port
+const PORT = process.env.PORT || 5000 //first looks for a PORT variable, if not present, uses the localhost port
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`)
