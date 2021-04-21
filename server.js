3c9e69ddcad605c1785feb9ece14498136f05845
Franco Traverso <franco-t20@outlook.com>
2021-04-21 20:48:44 -0300

    add routes to the API

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     res.json({ msg: 'ContactKeeper API'})
 })
 
+
+// Routes
+app.use('/api/users', require('./routes/users'))  //whenever some hits a specific endpoint, it will require its specific route file
+app.use('/api/auth', require('./routes/auth')) 
+app.use('/api/contacts', require('./routes/contacts'))
+
 const PORT = process.env.PORT || 5000 //first looks for a PORT variable, if not present, uses the localhost port
 
 app.listen(PORT, () => {
