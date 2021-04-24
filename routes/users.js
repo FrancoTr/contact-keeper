5c2c97e0b1cf141bf4deaf5df787eba7d61b6969
Franco Traverso <franco-t20@outlook.com>
2021-04-24 15:46:39 -0300

    verify if a user exists

--- routes/users.js
+++ routes/users.js
@@ -11,13 +11,29 @@ router.post('/', [
     check('name', 'Please add a name').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body
+    try {
+        let user = await User.findOne({ email })
+        
+        if(user) {
+            return res.status(400).json({ msg: 'User already exists'})
+        }
+
+        user = new User({   //It creates a new instance of a user, still not present on the DB
+            name,
+            email,
+            password
+        })
+        
+    } catch (err) {
+
+    }
 })
 
 module.exports = router
