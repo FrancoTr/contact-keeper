7c29b4ee2fdc131456544d410aa8c39383f51cb1
Franco Traverso <franco-t20@outlook.com>
2021-04-24 16:57:32 -0300

    add auth validation

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,45 @@ router.get('/', (req, res) => {
 // @desc    Auth user & get token
 // @access  Public
 
-router.post('/', (req, res) => {
-    res.send('Log in user')
+router.post('/', [
+    check('email', 'Please include a valid Email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+    try {
+        let user = await User.findOne({ email })
+
+        if(!user) {
+            return res.status(400).json({ msg: 'Invalid Credentials' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password) //compares the plain text password and the hashed password stored in the DB
+        if(!isMatch) {
+            return res.status(400).json({ msg: 'Invalid Credentials' })
+        }
+        const payload = {    //sent object inside the token
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 36000    //tweak to 360 (1 hour) when finished testing
+        }, (err, token) => {
+            if(err) {
+                throw err
+            } 
+            res.json({ token })
+        })
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router
