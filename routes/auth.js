0d151cf07c49a2e4de700c758a16306849758057
Franco Traverso <franco-t20@outlook.com>
2021-04-25 22:59:18 -0300

    get user data if the token is valid during the log in

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,14 @@ const User = require('../models/User')
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', auth, (req, res) => {
-    res.send('Get logged in user')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await (await User.findById(req.user.id)).isSelected('-password')   //gets all the data from User, sans the encripted password
+        res.json(user)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 // @route   POST api/auth
