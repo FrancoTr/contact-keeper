9e47dbe6984dad5637f956c4f1655d5c43f0b359
Franco Traverso <franco-t20@outlook.com>
2021-04-28 17:34:52 -0300

    contact update route

--- routes/contacts.js
+++ routes/contacts.js
@@ -50,8 +50,43 @@ router.post('/', [ auth, [ //the second argument goes in square brackets, 2 midd
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
-router.put('/:id', (req, res) => {
-    res.send('Update contact')
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body
+
+    // Build contact object
+    const contactFields = {}
+    if(name) {
+        contactFields.name = name
+    }
+    if(email) {
+        contactFields.email = email
+    }
+    if(phone) {
+        contactFields.phone = phonee
+    }
+    if(type) {
+        contactFields.type = type
+    }
+
+    try {
+        let contact = await Contact.findById(req.params.id)
+        if(!contact) {
+            return res.status(404).json({ msg: 'Contact not found' })
+        }
+        // Make sure user owns contact
+        if(contact.user.toString() !== req.user.id) {  //if the contact does not belong to the logged in user
+            return res.status(401).json({ msg: 'Not authorized' })
+        }
+
+        conctact = await Contact.findByIdAndUpdate(req.params.id, 
+            { $set: contactFields }, 
+            { new: true})
+            
+            res.json(contact)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 // @route   DELETE api/contacts/:id
