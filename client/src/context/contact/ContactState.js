1aecef1dabd628731a827464513293a13521dfb3
Franco Traverso <franco-t20@outlook.com>
2021-05-01 14:41:17 -0300

    add dummy data to the contactState

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -11,3 +11,32 @@ import {
     FILTER_CONTACTS,
     CLEAR_FILTER
 } from '../types'
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [ //hardcoded dummy contacts until the API is ready
+            {
+                id: 1,
+                name: 'Juancito Perez',
+                email: 'juancitoperez@gmail.com',
+                phone: '111-111-111-111',
+                type: 'presonal'
+            },
+            {
+                id:2,
+                name: 'Pepito Gomez',
+                email: 'pepitogomez@gmail.com',
+                phone: '222-222-222-222',
+                type: 'personal'
+            },
+            {
+                id: 3,
+                name: 'Fernando Fofo',
+                email: 'fernandofofo@gmail.com',
+                phone: '333-333-333-333',
+                type: 'professional'
+
+            }
+        ]    
+    }
+}
