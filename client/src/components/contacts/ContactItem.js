8334467c6381bd565193af101f27c80f5b50850e
Franco Traverso <franco-t20@outlook.com>
2021-05-04 19:48:31 -0300

    add email & phone info and items to the ContactItem component

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
 const ContactItem = ({ contact }) => {
-    const { id, name, email, phone, type } = contact //I pass the specific contact context as a prop
+    const { id, name, email, phone, type } = contact
     return (
         <div className='card bg-light'>
             <h3 className="text-primary text-left">
-                {name}{' '} <span className={'badge' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type}</span>
+                {name}{' '} 
+                <span style={{ float: 'right' }} className={'badge' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>
+                    {type.charAt(0).toUpperCase() + type.slice(1)}
+                </span>
             </h3>
+            <ul className="list">
+                {email && (<li>
+                    <i className="fas fa-envelope-open"></i> {email}
+                </li>)}
+                {phone && (<li>
+                    <i className="fas fa-phone"></i> {phone}
+                </li>)}
+            </ul>
         </div>
     )
 }
