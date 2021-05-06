c0c2683e67623fc41dee617233ea2728122fdea8
Franco Traverso <franco-t20@outlook.com>
2021-05-05 23:04:53 -0300

    fix Contacts Component

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -10,7 +10,7 @@ const Contacts = () => {
     return (
         <Fragment>
             {contacts.map(contact =>  (
-            <h3>{contact.name}</h3>
+                <ContactItem key={contact.id} contact={contact}/>
             ))}
         </Fragment>
     )
