793e85b4942eb9647b18a930b186d7712d072e39
Franco Traverso <franco-t20@outlook.com>
2021-05-06 21:42:46 -0300

    add onSubmit event in the Contact form

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -15,8 +15,19 @@ const ContactForm = () => {
 
     const onChange = e => setContact({ ...contact, [e.target.name] : e.target.value}) 
 
+    const onSubmit = e => {
+        e.preventDefault()
+        contactContext.addContact(contact)
+        setContact({    //I clear the form
+            name: '',
+            email: '',
+            phone: '',
+            type: 'personal'
+        })
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <h2 className="text-primary">Add Contact</h2>
             <input type="text" placeholder="Name" name="name" value={name} onChange={onChange}/>
             <input type="email" placeholder="Email" name="email" value={email} onChange={onChange}/>
