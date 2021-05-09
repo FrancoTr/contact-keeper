09913768bd73ea6fbbb4b18ae7771d709f5d57e0
Franco Traverso <franco-t20@outlook.com>
2021-05-08 23:47:29 -0300

    add setCurrent function to the onClick event (edit button)

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -4,7 +4,7 @@ import ContactContext from '../../context/contact/contactContext'
 
 const ContactItem = ({ contact }) => {
     const contactContext = useContext(ContactContext)
-    const { deleteContact } = contactContext
+    const { deleteContact, setCurrent } = contactContext
 
     const { id, name, email, phone, type } = contact
 
@@ -29,7 +29,7 @@ const ContactItem = ({ contact }) => {
                 </li>)}
             </ul>
             <p>
-                <button className='btn btn-dark btn-sm'>Edit</button>
+                <button className='btn btn-dark btn-sm' onClick={ () => setCurrent(contact)}>Edit</button>
                 <button className='btn btn-danger btn-sm' onClick={onDelete}>Delete</button>
             </p>
         </div>
