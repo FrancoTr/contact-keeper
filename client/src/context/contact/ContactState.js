05bd502261f0024368b109ed79633dad3d31804f
Franco Traverso <franco-t20@outlook.com>
2021-05-08 23:45:28 -0300

    add new functions to the contact state provider

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -74,8 +74,11 @@ const ContactState = props => {
         <ContactContext.Provider
             value={{
                 contacts: state.contacts,
+                current: state.current,
                 addContact,
-                deleteContact
+                deleteContact,
+                setCurrent,
+                clearCurrent
             }}>
             { props.children }
         </ContactContext.Provider>
