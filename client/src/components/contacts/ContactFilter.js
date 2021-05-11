c27649b62ebb58b4f14a65f7c04353fdd64cdf39
Franco Traverso <franco-t20@outlook.com>
2021-05-11 14:56:17 -0300

    add onChange and hooks functions to ContactFilter component

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,8 +1,25 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import ContactContext from '../../context/contact/contactContext'
 
 const ContactFilter = () => {
     const contactContext = useContext(ContactContext) 
+    const text = useRef('')
+
+    const { filterContacts, clearFilter, filtered } = contactContext
+
+    useEffect(() => {
+        if(filtered === null) {
+            text.current.value = ''
+        }
+    })
+
+    const onChange = e => {
+        if(text.current.value !== '') {
+            filterContacts(e.target) //filterContacts compares what was wrote in the filter with any current contact (name or email)
+        } else {
+            clearFilter()
+        }
+    }
     return (
         <form>
             <input ref={text} type="text" placeholder="Filter Contacts..." onChange={onChange}/>
