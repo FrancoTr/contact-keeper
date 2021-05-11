0ca5ef923532c45ad74d94f5c8fcf8ffa17b0f10
Franco Traverso <franco-t20@outlook.com>
2021-05-11 14:46:12 -0300

    add input element to the ContactFilter component

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
+import ContactContext from '../../context/contact/contactContext'
 
 const ContactFilter = () => {
+    const contactContext = useContext(ContactContext) 
     return (
-        <div>
-            
-        </div>
+        <form>
+            <input ref={text} type="text" placeholder="Filter Contacts..." onChange={onChange}/>
+        </form>
     )
 }
 
