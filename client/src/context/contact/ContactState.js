6c948343ff436ec0fd04500a8110889cbacec2a6
Franco Traverso <franco-t20@outlook.com>
2021-05-11 13:57:58 -0300

    add clearFilter function to the contact state

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -76,6 +76,9 @@ const ContactState = props => {
     }
 
     // Clear Filter
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER })
+    }
 
     return (
         <ContactContext.Provider
