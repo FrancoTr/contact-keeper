86d7a699ed527451e28b426550bece24da542573
Franco Traverso <franco-t20@outlook.com>
2021-05-15 14:22:38 -0300

    add onSubmit function to the register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -11,13 +11,18 @@ const Register = () => {
     const { name, email, password, password2 } = user
 
     const onChange = e => setUser({ ...user, [e.target.name]: e.target.value })
-    
+
+    const onSubmit = e => {
+        e.preventDefault()
+        console.log('Register submit')
+    }
+
     return (
         <div className='form-container'>
             <h1>
                 Account <span className='text-primary'>Register</span>
             </h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='form-group'>
                     <label htmlFor='name'>Name</label>
                     <input type='text' name='name' value={name} onChange={onChange}/>
