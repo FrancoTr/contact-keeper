e710efd7bef7ad8c9820da35349b765b0edf4a1f
Franco Traverso <franco-t20@outlook.com>
2021-05-18 21:41:21 -0300

    add register request to AuthState

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,49 +1,73 @@
-import React, { useReducer } from 'react'
-import AuthContext from './authContext'
-import authReducer from './authReducer'
+import React, { useReducer } from "react";
+import axios from "axios";
+import AuthContext from "./authContext";
+import authReducer from "./authReducer";
 import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    USER_LOADED,
-    AUTH_ERROR,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    CLEAR_ERRORS
-} from '../types'
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  CLEAR_ERRORS,
+} from "../types";
 
-const AuthState = props => {
-    const initialState = {
-        token: localStorage.getItem('token'),
-        isAuthenticated: null,
-        loading: true,
-        user: null,
-        error: null
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // Load User
+
+  // Register User
+  const register = async (formData) => {
+    // formData has all the required user info
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/users", formData, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg,
+      });
     }
+  };
 
-    const [state, dispatch] = useReducer(authReducer, initialState)
+  // Login User
 
-    // Load User
+  // Logout
 
-    // Register User
+  // Clear Errors
 
-    // Login User
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
-    // Logout
-
-    // Clear Errors
-
-    return (
-        <AuthContext.Provider
-            value={{
-                token: state.token,
-                isAuthenticated: state.isAuthenticated,
-                loading: state.loading,
-                user: state.user,
-                error: state.error,
-            }}>
-            { props.children }
-        </AuthContext.Provider>
-    )
-}
-
-export default AuthState
+export default AuthState;
