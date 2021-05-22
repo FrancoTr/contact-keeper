5601485476a0ef1bd119ef423e503be581a4c182
Franco Traverso <franco-t20@outlook.com>
2021-05-21 21:28:27 -0300

    add redirect to homepage effect if the user is already authenticated/logged in

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,20 +2,24 @@ import React, { useState, useContext, useEffect } from "react";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
-const Register = () => {
+const Register = (props) => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
   const { setAlert } = alertContext;
 
-  const { register, error, clearErrors } = authContext;
+  const { register, error, clearErrors, isAuthenticated } = authContext;
 
   useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/"); //redirect to the homepage
+    }
+
     if (error === "User already exists") {
       setAlert(error, "danger");
       clearErrors();
     }
-  }, [error]); // [error] is the dependency to useEffect
+  }, [error, isAuthenticated, props.history]); // [error] is the dependency to useEffect
 
   const [user, setUser] = useState({
     name: "",
