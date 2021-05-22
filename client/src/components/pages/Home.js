eb550d47499df37f6497f31d6eb98415a54d5d39
Franco Traverso <franco-t20@outlook.com>
2021-05-22 11:57:53 -0300

    run loadUser() in the homepage as soon as we log in

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import Contacts from '../contacts/Contacts'
-import ContactForm from '../contacts/ContactForm'
-import ContactFilter from '../contacts/ContactFilter'
+import React, { useContext, useEffect } from "react";
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/ContactFilter";
+import AuthContext from "../../context/auth/authContext";
 
 const Home = () => {
-    return (
-        <div className='grid-2'>
-            <div>
-                <ContactForm/>
-            </div>
-            <div>
-                <ContactFilter/>
-                <Contacts/>
-            </div>
-        </div>
-    )
-}
+  const authContext = useContext(AuthContext);
 
-export default Home
+  useEffect(() => {
+    //it runs as soon as the component loads
+    authContext.loadUser();
+  }, []);
+  return (
+    <div className="grid-2">
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <ContactFilter />
+        <Contacts />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
