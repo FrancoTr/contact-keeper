5837b0482a596ff43cacd5efd7f2aeec26d0ba95
Franco Traverso <franco-t20@outlook.com>
2021-05-24 11:34:53 -0300

    add login function on AuthState

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -71,7 +71,30 @@ const AuthState = (props) => {
   };
 
   // Login User
-  const login = () => console.log("login");
+  const login = async (formData) => {
+    // formData has all the required user info
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   // Logout
   const logout = () => console.log("logout");
