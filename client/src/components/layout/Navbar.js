2807ce72e12c87947d5bbd3394b4a6fb415da5c3
Franco Traverso <franco-t20@outlook.com>
2021-05-26 14:45:14 -0300

    add guest links to the navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,6 +20,17 @@ const Navbar = ({ title, icon }) => {
     </Fragment>
   );
 
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
