1c517626482ff412b8e60923e534839af3a501af
Franco Traverso <franco-t20@outlook.com>
2021-05-26 14:46:42 -0300

    change navbar link display whether the user is logged in or not

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -36,20 +36,7 @@ const Navbar = ({ title, icon }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
     </div>
   );
 };
