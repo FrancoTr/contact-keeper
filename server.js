453681da7034159c2ec44c0d712d7b8fbdde63ba
Franco Traverso <franco-t20@outlook.com>
2021-06-04 23:53:17 -0300

    remove response message when GET request

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const express = require('express')
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
-
-const app = express()
+const app = express();
 
 // Connect Database
-connectDB()
+connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.json({ msg: 'ContactKeeper API'})
-})
-
+})*/
 
 // Routes
-app.use('/api/users', require('./routes/users'))  //whenever some hits a specific endpoint, it will require its specific route file
-app.use('/api/auth', require('./routes/auth')) 
-app.use('/api/contacts', require('./routes/contacts'))
+app.use("/api/users", require("./routes/users")); //whenever some hits a specific endpoint, it will require its specific route file
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
-const PORT = process.env.PORT || 5000 //first looks for a PORT variable, if not present, uses the localhost port
+const PORT = process.env.PORT || 5000; //first looks for a PORT variable, if not present, uses the localhost port
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`)
-})
+  console.log(`Server started on ${PORT}`);
+});
